4afa0633226934055afe8cc4374272a016a02ef4
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-07-13 23:07:29 +0300

    finish the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
-
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -13,22 +13,27 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://starwars-api-section15-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,30 +44,45 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  // function fetchMoviesHandler() {
-  //   fetch("https://swapi.dev/api/films/")
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       const transformedMovies = data.results.map((movieData) => {
-  //         return {
-  //           id: movieData.episode_id,
-  //           title: movieData.title,
-  //           openingText: movieData.opening_crawl,
-  //           releaseDate: movieData.release_date,
-  //         };
-  //       });
-  //       setMovies(transformedMovies);
-  //     });
+  async function addMovieHandler(movie) {
+    const response = fetch(
+      "https://starwars-api-section15-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = (await response).json();
+    console.log(data);
+  }
+
+  // let content = <p>Found no movies.</p>;
+
+  // if (movies.length > 0) {
+  //   content = <MoviesList movies={movies} />;
+  // }
+
+  // if (error) {
+  //   content = <p>{error}</p>;
+  // }
+
+  // if (isLoading) {
+  //   content = <p>Loading...</p>;
   // }
 
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
+        {/* {content} */}
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
         {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
         {!isLoading && error && <p>{error}</p>}
